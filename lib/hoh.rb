d9f93c0c54249d7b5b430f34807e59f046cbcc49
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-25 23:44:46 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,7 +46,19 @@ def naming_system
     sub_category: {
      label: "Phylum",
      sub_category: {
-       
+       label: "Class",
+       sub_category:{
+        label:"Order",
+        sub_category:{
+          label:"Family",
+          sub_category:{
+            label: "Genus",
+            sub_category:{
+              label:"Species"
+            }
+          }
+        }
+       }
      }
     }
     
